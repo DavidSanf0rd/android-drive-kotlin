fc6ab583bcd3e820dfbbbdc1d3b3b0a874476371
David Sanford <dsanfordseu@gmail.com>
2017-03-11 12:52:29 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/sanf0rd/gdrive/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sanf0rd.gdrive
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/sanf0rd/gdrive/MainFragment.kt
@@ -0,0 +1,20 @@
+package com.example.sanf0rd.gdrive
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by sanf0rd on 11/03/17.
+ */
+class MainFragment: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
