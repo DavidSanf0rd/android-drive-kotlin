bddf8f70d4588d710121faf629f84fe5b3a6bca6
David Sanford <dsanfordseu@gmail.com>
2017-03-11 20:04:47 -0300

    Add second fragment

--- app/src/main/java/com/example/sanf0rd/gdrive/ScreenSlidePagerAdapter.kt
+++ app/src/main/java/com/example/sanf0rd/gdrive/ScreenSlidePagerAdapter.kt
@@ -10,16 +10,24 @@ import android.support.v4.app.FragmentStatePagerAdapter
 class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
 
     val fragment = MainFragment.newInstance(title = "Fragment 1")
+    val fragment2 = MainFragment.newInstance(title = "Fragment 2")
+
+    val fragments = arrayOf(fragment, fragment2)
 
     override fun getItem(position: Int): Fragment {
-        return fragment
+        return fragments[position]
     }
 
     override fun getCount(): Int {
-        return 1
+        return fragments.size
     }
 
     override fun getPageTitle(position: Int): CharSequence {
-        return "Fragment 1"
+        when (position) {
+            0 -> return "Fragment 1"
+            1 -> return "Fragment 2"
+        }
+
+        return "No title"
     }
 }
